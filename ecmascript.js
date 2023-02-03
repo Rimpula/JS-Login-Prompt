f146ce800c0fce96319e3305baab0809cb1dd095
Daniel Kyllonen <danielkyllonen89@gmail.com>
2023-02-03 17:41:42 +0200

    Code trivia prompt

---
+++ ecmascript.js
@@ -0,0 +1,8 @@
+let question = prompt("What's the \"official\" name of Javascript?");
+if (question === "ECMAScript") {
+    alert("Right!");
+} else if (question === "" || question === null) {
+    alert("You don't know? \"ECMAScript\"!");
+} else {
+    alert("You don't know? \"ECMAScript\"!");
+}
